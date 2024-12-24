46171be7d68740e065aed3c1483626e7f5bb7c5d
JamesTalamo <jamestalamo4@gmail.com>
2024-12-25 01:01:46 +0800

    update 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Page2 from "./pages/Page2"
 import Page3 from "./pages/Page3"
 import Page4 from "./pages/Page4"
 import Page5 from "./pages/Page5"
-import Footer from "./pages/footer"
+import Footer from "./pages/Footer"
 
 
 
